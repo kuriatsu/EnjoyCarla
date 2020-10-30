4e0cc5679c0d3f721c25ac595b3723e3aae9bb95
kuriatsu <kuriatsubayashi712@gmail.com>
2020-10-30 18:33:31 +0900

    tweek town5 scenario and launch scenario from roslaunch

---
+++ scripts/carla_scenario.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+scenarios=(
+"/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_base.xml"
+"/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_1.xml"
+)
+# args=""
+# for scenario in "${scenarios[@]}"; do
+#     args+=""
+#     args+="[ ${scenario} ]"
+# done
+
+python /home/kuriatsu/Source/carla_scenario/carla_ros_bridge.py -s "${scenarios[@]}"
