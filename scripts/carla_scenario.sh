6dbea9c7a42623ffc08c8ec91351e307ed2e408d
kuriatsu <kuriatsubayashi712@gmail.com>
2020-10-31 17:25:42 +0900

    add new scenario

--- scripts/carla_scenario.sh
+++ scripts/carla_scenario.sh
@@ -3,6 +3,7 @@
 scenarios=(
 "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_base.xml"
 "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_1.xml"
+"/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_2.xml"
 )
 # args=""
 # for scenario in "${scenarios[@]}"; do
