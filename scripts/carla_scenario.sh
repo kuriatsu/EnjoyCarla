46ded70b49d11af7a022b39f50fa20ea160cbb6f
kuriatsu <kuriatsubayashi712@gmail.com>
2020-11-17 21:00:52 +0900

    create scenario04

--- scripts/carla_scenario.sh
+++ scripts/carla_scenario.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-scenarios=(
-"/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_base.xml"
-"/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_1.xml"
-"/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_2.xml"
-)
+if [ $1 = "Town05" ]; then
+    scenarios=(
+    "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_base.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_1.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town5_simple_2.xml"
+    )
+    python /home/kuriatsu/Source/carla_scenario/carla_ros_bridge.py -s "${scenarios[@]}"
+
+elif [ $1 = "Town04" ]; then
+    scenarios=(
+    "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town4.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/scenario_town4_intervene.xml"
+    )
+    python /home/kuriatsu/Source/carla_scenario/carla_ros_bridge.py -s "${scenarios[@]}"
+
+else
+    echo "no town specified"
+fi
 # args=""
 # for scenario in "${scenarios[@]}"; do
 #     args+=""
 #     args+="[ ${scenario} ]"
 # done
-
-python /home/kuriatsu/Source/carla_scenario/carla_ros_bridge.py -s "${scenarios[@]}"
--- src/viz_start_goal.cpp
+++ src/viz_start_goal.cpp
@@ -54,7 +54,7 @@ geometry_msgs::Pose VizStartGoal::gerPoseFromParam(const std::string &string)
 		out_pose.position.x = std::stof(buf_list.at(0));
 		out_pose.position.y = -std::stof(buf_list.at(1));
 		out_pose.position.z = std::stof(buf_list.at(2));
-		buf_quat.setRPY(0.0, 0.0, std::stof(buf_list.at(3)));
+		buf_quat.setRPY(M_PI*0.5, 0.0, std::stof(buf_list.at(3)) * M_PI /(180.0));
 		out_pose.orientation = tf2::toMsg(buf_quat);
 	}
 
@@ -72,48 +72,52 @@ void VizStartGoal::initPictogram(const geometry_msgs::Pose &start, const geometr
 	pictogram.action = jsk_rviz_plugins::Pictogram::ADD;
 	pictogram.ttl = 1.0;
 	pictogram.color.a = 1.0;
-	rot_tf_quat.setRPY(0.0, 90.0, 0.0);
+	// rot_tf_quat.setRPY(0.0, M_PI*0.5, 0.0);
 
 	// start icons
 	pictogram.pose = start;
 	pictogram.color.g = 1.0;
 
+    pictogram.pose.orientation.x = 0;
+    pictogram.pose.orientation.y = 0;
 	pictogram.pose.orientation.w = 1;
 	pictogram.pose.orientation.z = 1;
 	pictogram.mode = jsk_rviz_plugins::Pictogram::PICTOGRAM_MODE;
 	pictogram.character = "fa-circle";
-	pictogram.size = 1.0;
+	pictogram.size = 10.0;
 	m_pictogram_array.pictograms.push_back(pictogram);
 
-    // orig_tf_quat = tf2::fromMsg(start.orientation);
-    tf2::fromMsg(start.orientation, orig_tf_quat);
-	tf2::fromMsg(start.orientation, orig_tf_quat);
-	new_tf_quat = rot_tf_quat * orig_tf_quat;
-	pictogram.pose.orientation = tf2::toMsg(new_tf_quat);
+    tf2::fromMsg(pictogram.pose.orientation, orig_tf_quat);
+	tf2::fromMsg(start.orientation, rot_tf_quat);
+    new_tf_quat = rot_tf_quat * orig_tf_quat;
+    pictogram.pose.orientation = tf2::toMsg(new_tf_quat);
+    pictogram.pose.position.z += 2;
 	pictogram.mode = jsk_rviz_plugins::Pictogram::STRING_MODE;
 	pictogram.character = "START";
-	pictogram.size = 1.0;
+	pictogram.size = 5.0;
 	m_pictogram_array.pictograms.push_back(pictogram);
 
 	// goal icons
 	pictogram.pose = goal;
 	pictogram.color.r = 1.0;
 
+    pictogram.pose.orientation.x = 0;
+    pictogram.pose.orientation.y = 0;
 	pictogram.pose.orientation.w = 1;
-	pictogram.pose.orientation.y = 1;
+    pictogram.pose.orientation.z = 1;
 	pictogram.mode = jsk_rviz_plugins::Pictogram::PICTOGRAM_MODE;
 	pictogram.character = "fa-circle";
-	pictogram.size = 1.0;
+	pictogram.size = 10.0;
 	m_pictogram_array.pictograms.push_back(pictogram);
 
-	// orig_tf_quat = tf2::fromMsg(goal.orientation);
-    tf2::fromMsg(start.orientation, orig_tf_quat);
-	new_tf_quat = rot_tf_quat * orig_tf_quat;
+    tf2::fromMsg(pictogram.pose.orientation, orig_tf_quat);
+    tf2::fromMsg(goal.orientation, rot_tf_quat);
+    new_tf_quat = rot_tf_quat * orig_tf_quat;
     pictogram.pose.orientation = tf2::toMsg(new_tf_quat);
-    // tf2::toMsg(new_tf_quat, pictogram.pose.orientation);
+    pictogram.pose.position.z += 2;
 	pictogram.mode = jsk_rviz_plugins::Pictogram::STRING_MODE;
 	pictogram.character = "GOAL";
-	pictogram.size = 1.0;
+	pictogram.size = 5.0;
 	m_pictogram_array.pictograms.push_back(pictogram);
 }
 
