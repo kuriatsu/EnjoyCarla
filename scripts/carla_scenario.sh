6deeb501406d9bc286b3dd23ef88263864fd4210
kuriatsu <kuriatsubayashi712@gmail.com>
2020-11-19 21:39:25 +0900

    change scenario running and default scenario

--- scripts/carla_scenario.sh
+++ scripts/carla_scenario.sh
@@ -2,17 +2,26 @@
 
 if [ $1 = "Town05" ]; then
     scenarios=(
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_static1.xml"
+    # "/home/kuriatsu/Source/carla_scenario/scenario/town5_base.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_cross0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_stands0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_static0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_stands0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_cross0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_static0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_static0.xml"
+    "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_stands0.xml"
     "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_cross0.xml"
     # "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_cross1.xml"
-    "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_stands0.xml"
     # "/home/kuriatsu/Source/carla_scenario/scenario/town5_pedestrian_stands1.xml"
-    "/home/kuriatsu/Source/carla_scenario/scenario/town5_static0.xml"
     # "/home/kuriatsu/Source/carla_scenario/scenario/town5_static1.xml"
     )
     python /home/kuriatsu/Source/carla_scenario/carla_ros_bridge.py -s "${scenarios[@]}"
 
 elif [ $1 = "Town04" ]; then
     scenarios=(
+    "/home/kuriatsu/Source/carla_scenario/scenario/town4_intervene.xml"
     "/home/kuriatsu/Source/carla_scenario/scenario/town4_base.xml"
     "/home/kuriatsu/Source/carla_scenario/scenario/town4_intervene.xml"
     "/home/kuriatsu/Source/carla_scenario/scenario/town4_intervene.xml"
